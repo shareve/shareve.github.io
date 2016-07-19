73910d13f6a60db1ea02424e50efb7783d59d5e7
changjun.zhu <changjun.zhu@dcfservice.net>
2016-07-19 10:03:04 +0800

    analytics_baidu.js

---
+++ javascripts/common/analytics_baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?09db7692ff30d9fcd3f5791f1aa69320";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
