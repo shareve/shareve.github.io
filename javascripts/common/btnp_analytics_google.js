55faea0ee74a8d770d61294cfece1d2a5bfe8c7e
changjun.zhu <changjun.zhu@dcfservice.net>
2017-02-28 10:13:50 +0800

    btnp analytics

---
+++ javascripts/common/btnp_analytics_google.js
@@ -0,0 +1,15 @@
+window.onload = function(){  //这里的代码将在页面准备就绪之后开始执行
+(function(i, s, o, g, r, a, m) {
+	i['GoogleAnalyticsObject'] = r;
+	i[r] = i[r] || function() {
+		(i[r].q = i[r].q || []).push(arguments)
+	}, i[r].l = 1 * new Date();
+	a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+	a.async = 1;
+	a.src = g;
+	m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js',
+		'ga');
+ga('create', 'UA-92710722-1', 'auto');
+ga('send', 'pageview');
+}
