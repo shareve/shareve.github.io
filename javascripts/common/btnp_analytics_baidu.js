33c8c1aba4ef33bcf7bc2d5dadc4b33ddcc8a0ed
shareve.zhu <shareve.zhu@gmail.com>
2018-09-12 22:36:10 +0800

    analytics

--- javascripts/common/btnp_analytics_baidu.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?08909d523995b9307ac2c6196514fe55";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
