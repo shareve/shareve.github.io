99d38894874a1aabb6a18ac1aca8648b9844c4cb
shareve <shareve.zhu@gmail.com>
2018-09-12 22:24:03 +0800

    Create bd_muluu_com.js
    bd_muluu_com.js

---
+++ javascripts/common/bd_muluu_com.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?e0d2be8f76e8d14a3326ef5dd06f5e57";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
